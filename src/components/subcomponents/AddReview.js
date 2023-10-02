887e01c70b6ef4a0f3c895c917821120c373a997
Restyled.io <commits@restyled.io>
2023-10-02 17:15:44 +0000

    Restyled by prettier

--- src/components/subcomponents/AddReview.js
+++ src/components/subcomponents/AddReview.js
@@ -1,5 +1,9 @@
-const AddReview =
-    () => { return (<div><h1>Page left Blank on purpose</h1>
-		</div>); };
+const AddReview = () => {
+	return (
+		<div>
+			<h1>Page left Blank on purpose</h1>
+		</div>
+	);
+};
 
 export default AddReview;
