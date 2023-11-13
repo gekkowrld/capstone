d7d11c96703c3f8039c21557615100d611f83f1c
Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
2023-11-13 11:15:00 +0300

    Migrate to books collection from products (#22)
    * chore: Partially migrate to "books"
    
    * fix: package.json & package-lock.json to reduce vulnerabilities
    
    The following vulnerabilities are fixed with an upgrade:
    - https://snyk.io/vuln/SNYK-JS-AXIOS-6032459
    
    * fix: upgrade @mui/icons-material from 5.14.11 to 5.14.12
    
    Snyk has created this PR to upgrade @mui/icons-material from 5.14.11 to 5.14.12.
    
    See this package in npm:
    https://www.npmjs.com/package/@mui/icons-material
    
    See this project in Snyk:
    https://app.snyk.io/org/gekkowrld/project/7cdca2b9-7787-4fd5-b6db-55c1b52fc3ec?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade @mui/styled-engine-sc from 5.14.11 to 5.14.12
    
    Snyk has created this PR to upgrade @mui/styled-engine-sc from 5.14.11 to 5.14.12.
    
    See this package in npm:
    https://www.npmjs.com/package/@mui/styled-engine-sc
    
    See this project in Snyk:
    https://app.snyk.io/org/gekkowrld/project/7cdca2b9-7787-4fd5-b6db-55c1b52fc3ec?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * fix: upgrade @mui/material from 5.14.11 to 5.14.12
    
    Snyk has created this PR to upgrade @mui/material from 5.14.11 to 5.14.12.
    
    See this package in npm:
    https://www.npmjs.com/package/@mui/material
    
    See this project in Snyk:
    https://app.snyk.io/org/gekkowrld/project/7cdca2b9-7787-4fd5-b6db-55c1b52fc3ec?utm_source=github&utm_medium=referral&page=upgrade-pr
    
    * chore(deps): bump browserify-sign from 4.2.1 to 4.2.2
    
    Bumps [browserify-sign](https://github.com/crypto-browserify/browserify-sign) from 4.2.1 to 4.2.2.
    - [Changelog](https://github.com/browserify/browserify-sign/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/crypto-browserify/browserify-sign/compare/v4.2.1...v4.2.2)
    
    ---
    updated-dependencies:
    - dependency-name: browserify-sign
      dependency-type: indirect
    ...
    
    * chore(deps): Apply security patches
    
    * chore: Remove git_helper script
    
    * chore: Add a skeleton Home page
    
    ---------
    
    Signed-off-by: Gekko Wrld <108436933+gekkowrld@users.noreply.github.com>
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: snyk-bot <snyk-bot@snyk.io>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/assets/img.js
+++ src/assets/img.js
@@ -5,6 +5,7 @@ import logo from "./Logo.png";
 import cartImgL from "./cart_ok.svg";
 import loadingCircle from "./circleLoading.svg";
 import loadingSquare from "./load_data_square.svg";
+import masterHeaderBG from "./masthead_background.jpg";
 
 export {
 	cartIMgX,
@@ -13,5 +14,6 @@ export {
 	logo,
 	cartImgL,
 	loadingCircle,
-	loadingSquare
+	loadingSquare,
+	masterHeaderBG
 };
